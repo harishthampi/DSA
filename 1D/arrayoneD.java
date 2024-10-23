20d08bf2f1a32bc61c8399bba5f3c2bda7f736e0
Harish Thampi <harishbhupeshthampi@gmail.com>
2024-10-23 08:07:54 -0400

    1D-Beginner

--- 1D/arrayoneD.java
+++ 1D/arrayoneD.java
@@ -137,3 +137,48 @@ public class Solution {
         return count;
     }
 }
+
+Q3. Frequency count
+Problem Description
+
+You are given an integer array A, you have to return an integer array of same size whose ith element is the frequency count of A[i] in array A .
+
+Problem Constraints
+1 <= len(A) <= 1000
+1 <= A[i] <= 100
+
+Input Format
+First argument is an array of integers representing array A.
+
+Output Format
+You have to return an array of integers as per the question.
+
+
+
+Example Input
+Input 1:
+A = [1, 2, 5, 1, 5, 1 ]
+
+
+Example Output
+Output 1:
+[3, 1, 2, 3, 2, 3]
+
+Example Explanation
+Explanation 1:
+Clearly, In the given array we have 1 three times, 2 one time and 5 two times.
+
+Code
+public class Solution {
+    public int[] solve(int[] A) {
+        int [] output = new int[A.length];
+        for(int i=0;i<A.length;i++){
+            int count=0;
+            for(int j=0;j<A.length;j++){
+                if(A[i]==A[j]) count++;
+            }
+            output[i]=count;
+        }
+        return output;
+    }
+}
