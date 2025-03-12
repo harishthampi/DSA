b187a32342a785c2cbe67d07ce44f32512c5dc58
Harish Thampi <harishbhupeshthampi@gmail.com>
2025-03-12 14:39:13 -0400

    Q2

--- 1D/Array/carryForward.java
+++ 1D/Array/carryForward.java
@@ -91,3 +91,95 @@ public class Solution {
         
 }
 
+*************************************************************************
+Q2. Closest MinMax
+Problem Description
+
+Given an array A, find the size of the smallest subarray such that it contains at least one occurrence of the maximum value of the array
+
+and at least one occurrence of the minimum value of the array.
+
+
+
+Problem Constraints
+
+1 <= |A| <= 2000
+
+
+
+Input Format
+
+First and only argument is vector A
+
+
+
+Output Format
+
+Return the length of the smallest subarray which has at least one occurrence of minimum and maximum element of the array
+
+
+
+Example Input
+
+Input 1:
+
+A = [1, 3, 2]
+Input 2:
+
+A = [2, 6, 1, 6, 9]
+
+
+Example Output
+
+Output 1:
+
+ 2
+Output 2:
+
+ 3
+
+
+Example Explanation
+
+Explanation 1:
+
+ Take the 1st and 2nd elements as they are the minimum and maximum elements respectievly.
+Explanation 2:
+
+ Take the last 3 elements of the array.
+
+ public class Solution {
+    public int solve(int[] A) {
+        int max_i = -1;
+        int min_i = -1;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0;i<A.length;i++){
+            if(A[i]> max){
+                max = A[i];
+            }
+            if(A[i]<min){
+                min = A[i];
+            }
+        }
+
+        for(int i=A.length-1;i>=0;i--){
+            if(A[i] == max){
+                max_i = i;
+                if(min_i != -1){
+                    ans = Math.min(ans,min_i - max_i + 1);
+                }
+            }
+            if(A[i] == min){
+                min_i = i;
+                if(max_i != -1){
+                    ans = Math.min(ans,max_i - min_i + 1);
+                }
+            }
+        }
+        return ans;
+
+    }
+}
